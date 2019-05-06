52fbd56b26adcd96068692604eebca1fdf51a0e9
David Gunawan <david_gunawan_@hotmail.com>
2019-05-06 08:33:33 +0700

    test

---
+++ AnotherBMI/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  AnotherBMI
+//
+//  Created by David Gunawan on 06/05/19.
+//  Copyright © 2019 David Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter themePicked: <#themePicked description#>
+    private func changeTheme(isLightSelected themePicked: Bool) {
+        if themePicked {
+            
+        } else {
+            
+        }
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter isWhite: <#isWhite description#>
+    private func setLabelColor(changeToWhite isWhite: Bool) {
+        if isWhite {
+            
+        } else {
+            
+        }
+    }
+    
+    
+    /// <#Description#>
+    private func calculateBMI() {
+        
+    }
+    
+    
+    /// <#Description#>
+    ///
+    /// - Parameter bmi: <#bmi description#>
+    private func changeStatus(bmi: Float) {
+        
+    }
+}
+
